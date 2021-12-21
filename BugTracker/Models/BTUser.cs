388ffb816eca9d3518a9f25ca328eccd1a44d80f
edkim3 <94409377+edkim3@users.noreply.github.com>
2021-12-21 12:52:34 -0700

    The Data Models
    Update BTUser model for ProjectMembers

--- BugTracker/Models/BTUser.cs
+++ BugTracker/Models/BTUser.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -21,6 +23,26 @@ namespace BugTracker.Models
         [NotMapped]
         [Display(Name = "Full Name")]
         public string FullName { get { return $"{FirstName} {LastName}"; } }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile AvatarFormFile { get; set; }
+
+        [DisplayName("File Name")]
+        public string AvatarFileName { get; set; }
+
+        public byte[] AvatarFileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string AvatarContentType { get; set; }
+
+        public int? CompanyId { get; set; }
+
+        // Navigation Properties
+
+        public virtual Company Company { get; set; }
+
+        public virtual ICollection<Project> Projects { get; set; }
 
     }
 }
