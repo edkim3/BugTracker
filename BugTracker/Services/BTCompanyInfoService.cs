6639ad4186295359e71d713c6f6af804329e1b89
edkim3 <94409377+edkim3@users.noreply.github.com>
2021-12-22 15:55:23 -0700

    CompanyInfo Service (part 2)
    Create BTCompanyInfoService class. Implement IBTCompanyInfoService interface. Use dependency injection. Update the GetAllMembersAsync method.

---
+++ BugTracker/Services/BTCompanyInfoService.cs
@@ -0,0 +1,43 @@
+﻿using BugTracker.Data;
+using BugTracker.Models;
+using BugTracker.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Services
+{
+    public class BTCompanyInfoService : IBTCompanyInfoService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BTCompanyInfoService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<BTUser>> GetAllMembersAsync(int companyId)
+        {
+            List<BTUser> result = new();
+            result = await _context.Users.Where(u => u.CompanyId == companyId).ToListAsync();
+            return result;
+        }
+
+        public Task<List<Project>> GetAllProjectsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company> GetCompanyInfoByIdAsync(int? companyId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
